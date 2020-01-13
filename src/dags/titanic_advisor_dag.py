b87c4b0eaa824c42a307bcfaa84fb09fb38aaaee
Adesh Shendge <adesh@Adeshs-MacBook-Pro.local>
2020-01-13 22:19:02 +0000

    Added Code to run DAGS

---
+++ src/dags/titanic_advisor_dag.py
@@ -0,0 +1,29 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+
+
+default_args = {
+    'owner': 'Adesh',
+    'depends_on_past': False,
+    'start_date': datetime(2015, 6, 1),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+}
+
+dag = DAG('Tianic_Data_Advisor', default_args=default_args, schedule_interval=timedelta(days=1))
+
+# t1, t2 and t3 are examples of tasks created by instantiating operators
+t1 = BashOperator(
+    task_id='Tianic_Data_Advisor',
+    bash_command='docker run -it --link e5a0d72e8381:27017 --link b7398774a8ba:8529 ml_base python src/advisor/titanic_advisor.py',
+    dag=dag)
+
+t1
